11811d4f3201cdb94a9de1bc757f6b5f5e012c5d
varshita94 <238w1a1294@vrsec.ac.in>
2026-04-16 14:32:15 +0530

    Added Login functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Register from './components/Register'
-
+import Login from './components/Login'
 export default function App() {
   return (
     <BrowserRouter>
-      <Navbar/>
+    <Navbar/>
       <Routes>
-        <Route path="/" element />
+        <Route path="/login" element={<Login/>}/>
         <Route path='/register' element={<Register/>}/>
       </Routes>
     </BrowserRouter>
