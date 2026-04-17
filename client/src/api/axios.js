9c1162b257ac0ee8d70c3a48bfc2eb244bf9f144
varshita94 <238w1a1294@vrsec.ac.in>
2026-04-17 16:28:21 +0530

    added cart

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const API = axios.create({
-    baseURL:"http://localhost:5000/api"
+    baseURL:"https://mern-ecommerce-lqyb.onrender.com"
 })
 API.interceptors.request.use((req)=>{
     const token=localStorage.getItem("token")
